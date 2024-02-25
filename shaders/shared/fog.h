616b20d2e395f3279d5f2858aa4722a888bdaf73
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-02-25 23:03:55 +0400

    Исправление тумана на Forward объектах

--- shaders/shared/fog.h
+++ shaders/shared/fog.h
@@ -11,28 +11,21 @@
 #define FOG_DENSITY_MULTIPLIER 0.02f
 #define VERTICAL_FOG_DENSITY_MULTIPLIER 0.1f
 ////////////////////////////////////////////////////////////////////////////
-float CalcSqaredExponentialFogness(float3 Position)
+float CalcSqaredExponentialFog(float3 Position)
 {
 	float Distance = length(Position);
-	float FogDensity = fog_density * FOG_DENSITY_MULTIPLIER;
+	float FogDensity = fog_params.a * FOG_DENSITY_MULTIPLIER;
 	return saturate(1.0h - exp((-Distance * FogDensity) * (Distance * FogDensity)));
 }
 
 float CalcVertexFogness(float3 Position)
 {
-	return CalcSqaredExponentialFogness(Position - eye_position);
+	return CalcSqaredExponentialFog(Position - eye_position);
 }
 
-void ApplyVertexFogness(inout float3 Color, float VertexFog)
+float CalcFogness(float3 Position)
 {
-	Color = lerp(Color, fog_color, VertexFog);
-}
-
-void ApplyPixelFogness(inout float3 Color, inout float Alpha, float3 Position)
-{
-	float Fog = CalcSqaredExponentialFogness(Position);
-	Color = lerp(Color, fog_color, Fog);
-	Alpha *= sqrt(Fog);
+	return CalcSqaredExponentialFog(Position);
 }
 ////////////////////////////////////////////////////////////////////////////
 #endif//FOG_INCLUDED
