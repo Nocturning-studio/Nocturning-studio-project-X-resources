2d1b817db1984465210a3647e63665eea7248d17
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-03-09 23:35:17 +0400

    Сепия, Винъетирование и цветовые абберации перенесены в postprocess.ps

--- shaders/r2/postprocess.s
+++ shaders/r2/postprocess.s
@@ -1,9 +1,9 @@
-
-function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("null","postprocess")
-			: fog	(false)
-			: zb 	(false,false)
-	shader:sampler	("s_base0")	:texture("$user$generic0")	: clamp()
-	shader:sampler	("s_base1")    	:texture("$user$generic0")	: clamp()
-	shader:sampler	("s_noise")    	:texture("fx\\fx_noise2")	: f_linear ()
+function normal(shader, t_base, t_second, t_detail)
+	shader:begin("null","postprocess")
+			: fog(false)
+			: zb (false,false)
+	shader:sampler("s_base0"):texture("$user$generic0"):clamp()
+	shader:sampler("s_base1"):texture("$user$generic0"):clamp()
+	shader:sampler("s_noise"):texture("fx\\fx_noise2"):f_linear ()
+	shader:sampler("s_vignette"):texture("vfx\\vfx_vignette"):f_linear ()
 end
