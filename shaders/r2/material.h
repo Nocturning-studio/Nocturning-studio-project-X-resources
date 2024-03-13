3099ac739cd73a629968621dd918f84d2cd5bb9d
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-03-13 14:34:11 +0400

    Переработка работы с материалами

--- shaders/r2/material.h
+++ shaders/r2/material.h
@@ -32,7 +32,9 @@ MaterialParams GetMaterial(float2 UV, float3x3 TBN, float3 Position)
     MaterialParams Material;
 
     // Update UV with displacement
+#ifdef USE_BUMP
     UV = GetDisplacement(s_bumpX, Position, TBN, UV);
+#endif
 
 #if defined(USE_ALPHA_TEST) && (ALPHA_TEST_AA == FXAA_ATOC)
     // Get Albedo and opacity with fxaa
@@ -43,6 +45,7 @@ MaterialParams GetMaterial(float2 UV, float3x3 TBN, float3 Position)
     Material.Opacity = BaseTextureData.a;
 #endif//USE_ALPHA_TEST && (ALPHA_TEST_AA == FXAA_ATOC)
 
+#ifdef USE_BUMP
     // Get DXT comressed normals + AO + glossmap
     float4 NormalMapData = tex2D(s_bump, UV);
 
@@ -55,26 +58,35 @@ MaterialParams GetMaterial(float2 UV, float3x3 TBN, float3 Position)
     // Reconstruct z component for normals (actually z component used for Baked AO)
     Material.Normal.z = sqrt(1.0f - dot(Material.Normal.xy, Material.Normal.xy));
 
-    Material.AO = tex2D(s_baked_ao, UV).r;
-
-    //Material.AO *= NormalMapData.g;
-
     Material.Height = NormalMapDecompressionData.a;
 
     Material.Glossiness = NormalMapData.r;
+#else//USE_BUMP
+    Material.Normal = float3(0.0f, 0.0f, 1.0f);
+    Material.Height = 1.0f;
+    Material.Glossiness = 0.0f;
+#endif//USE_BUMP
 
-    // If for material used detail texture - we use this block with analogic code, but with detail textures
+#ifdef USE_BAKED_AO
+    Material.AO = tex2D(s_baked_ao, UV).r;
+#else
+    Material.AO = 1.0f;
+#endif
+
 #ifdef USE_TDETAIL
     // Create detail UV - dt_params is scaler
     float2 DetailUV = UV * dt_params;
 
     // Calculate displacement for detail texture only if we have good quality for materials
-#if BUMP_QUALITY >= MIDDLE_QUALITY
+#if DETAIL_BUMP && BUMP_QUALITY >= MIDDLE_QUALITY
     DetailUV = GetDetailDisplacement(s_detailBumpX, Position, TBN, DetailUV);
-#endif
+#endif // DETAIL_BUMP && BUMP_QUALITY >= MIDDLE_QUALITY
 
-    // Get albedo, comressed normals + AO + glossmap, and decompressing data + heightmap
+    // Combine main albedo with detail
     float3 DetailAlbedo = tex2D(s_detail, DetailUV);
+    Material.Albedo.rgb *= 2.0f * DetailAlbedo;
+
+#ifdef USE_DETAIL_BUMP
     float4 DetailNormalMapData = tex2D(s_detailBump, DetailUV);
     float4 DetailNormalMapDecompressionData = tex2D(s_detailBumpX, DetailUV);
 
@@ -87,16 +99,15 @@ MaterialParams GetMaterial(float2 UV, float3x3 TBN, float3 Position)
     // Correctly blend two maps
     Material.Normal = Blend_Normal_Maps(Material.Normal, DetailNormal);
 
-    // Combine main albedo with detail
-    Material.Albedo.rgb *= 2.0f * DetailAlbedo;
-
     // Combine main height map with detail height map, multiplicated by coeffient, make detail height map influence is
     // smaller
     Material.Height += DetailNormalMapDecompressionData.a * 0.2f;
 
     Material.Glossiness = DetailNormalMapData.r * Material.Glossiness + Material.Glossiness;
+#endif // DETAIL_BUMP
 #endif // USE_TDETAIL
 
+#if defined(USE_BUMP) || defined(USE_DETAIL_BUMP)
     // If we have good material quality - create MORE relief by convertation height to normal map
 #if BUMP_QUALITY == ULTRA_QUALITY
     float3 NormalFromHeight = convert_height_to_normal(Material.Height);
@@ -108,7 +119,8 @@ MaterialParams GetMaterial(float2 UV, float3x3 TBN, float3 Position)
 
     // Make normals more power with heightmap
     Material.Normal.z *= Material.Height;
-    
+#endif//defined(USE_BUMP) || defined(USE_DETAIL_BUMP)
+
     Material.Normal = normalize(Material.Normal);
 
     return Material;
