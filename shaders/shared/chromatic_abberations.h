ee2aac7b5d1e6737060cb1fc73d54fb8b54d9980
Deathman00 <104826306+Deathman00@users.noreply.github.com>
2024-03-16 02:02:36 +0400

    Начало работы по переводу шейдеров на SM4.0/SM5.0

--- shaders/shared/chromatic_abberations.h
+++ shaders/shared/chromatic_abberations.h
@@ -9,17 +9,21 @@
 #define fFalloffRadius 1.8f                    // over this radius the effects is maximal
 #define fChromaPower 0.5f
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-float3 ChromaticAberration(type_sampler2D(Image), float3 ImageColor, float2 tex)
+float3 ChromaticAberration(texture2D ImageTexture, float3 ImageColor, float2 TexCoords)
 {
-    float d = distance(tex, float2(0.5f, 0.5f));
+    float d = distance(TexCoords, float2(0.5f, 0.5f));
     float f = smoothstep(fBaseRadius, fFalloffRadius, d + 0.075f * d);
     float3 chroma = pow(f + fvChroma, fChromaPower);
 
-    float2 tr = ((2.0f * tex - 1.0f) * chroma.r) * 0.5f + 0.5f;
-    float2 tg = ((2.0f * tex - 1.0f) * chroma.g) * 0.5f + 0.5f;
-    float2 tb = ((2.0f * tex - 1.0f) * chroma.b) * 0.5f + 0.5f;
+    float2 tr = ((2.0f * TexCoords - 1.0f) * chroma.r) * 0.5f + 0.5f;
+    float2 tg = ((2.0f * TexCoords - 1.0f) * chroma.g) * 0.5f + 0.5f;
+    float2 tb = ((2.0f * TexCoords - 1.0f) * chroma.b) * 0.5f + 0.5f;
 
-    float3 color = float3(tex2D(Image, tr).r, tex2D(Image, tg).g, tex2D(Image, tb).b) * (1.0f - f);
+    float3 color;
+    color.x = ImageTexture.Sample(smp_nofilter, tr).r;
+    color.y = ImageTexture.Sample(smp_nofilter, tg).g;
+    color.z = ImageTexture.Sample(smp_nofilter, tb).b;
+    color *= (1.0f - f);
 
     return lerp(ImageColor, color, 0.35f);
 }
