88e93eb3d83165ec5669fd9f33b28e547e02514b
mihan-323 <81863440+mihan-323@users.noreply.github.com>
2024-05-01 15:53:41 +0400

    Фиксы

--- shaders/r1/distort.s
+++ shaders/r1/distort.s
@@ -4,6 +4,6 @@ function normal		(shader, t_base, t_second, t_detail)
 			: fog	(false)
 			: zb 	(false,false)
 	shader:sampler	("s_distort")  	:texture(t_distort)	: clamp() : f_linear ()
-	shader:sampler	("s_base")	:texture(t_rt)		: clamp() : f_linear ()
+	shader:sampler	("s_image")	:texture(t_rt)		: clamp() : f_linear ()
 end
 
--- shaders/r1/postprocess.s
+++ shaders/r1/postprocess.s
@@ -1,10 +1,13 @@
 -- normal pp
+-- DX11: disabled
+--[[
 function normal	(shader, t_base, t_second, t_detail)
 	shader:begin("stub_notransform_postpr","postprocess")
 			: fog(false)
 			: zb (false,false)
-	shader:sampler("s_base") :texture(t_rt) : clamp() : f_linear ()
+	shader:sampler("s_image") :texture(t_rt) : clamp() : f_linear ()
 	shader:sampler("s_depth") :texture("$user$depth") : clamp() : f_linear ()
 	shader:sampler("s_noise") :texture(t_noise) : f_linear ()
 	shader:sampler("s_vignette") :texture("vfx\\vfx_vignette") : f_linear ()
 end
+]]
