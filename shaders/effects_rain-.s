23f31386c1703198999a3682d23ff8161f2d85e7
NSDeathman <Forest.Kir.325@gmail.com>
2024-12-19 00:53:13 +0400

    Отключен свой шейдер для дождя

--- shaders/effects_rain-.s
+++ shaders/effects_rain-.s
@@ -4,7 +4,7 @@ function normal(shader, t_base, t_second, t_detail)
 		: blend(true, blend.srcalpha, blend.one)
 		: aref(true, 0)
 		: sorting(0, false)
-		: zb(false, false)
+		: zb(true, false)
 
 	shader:sampler("s_base") 
         : texture(t_base) 
